1dbd9c421336dedb6b673e2022bcdef737863ac2
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-09-04 17:14:12 +0200

    esercizio completato

--- script.js
+++ script.js
@@ -9,13 +9,43 @@ let bowling = {
         this.players.forEach( (player)=> console.log(`# ${player.name} ===> Punteggio attuale: ${player.scores}`));
     },
     'gameSimulator' : function () {
-        let punteggi = this.players.forEach( (players)=> {for (let i = 1; i <= 10 ; i++) {
-            this.players.push ( (player)=> Math.floor(Math.random() * (10 - 1 +1) + 1));
-    }});
-        return console.log(punteggi);            
+        this.players.forEach( (player)=>{ 
+            for (let i = 1; i <= 10 ; i++) {
+                player.scores.push (Math.floor(Math.random() * (10 - 1 +1) + 1));
+            }            
+        })            
+    },
+    'addPlayer' : function (nome) {
+        let newPlayer = {name : nome, scores : []}
+        for (let i = 1; i <= 10 ; i++) {
+            newPlayer.scores.push (Math.floor(Math.random() * (10 - 1 +1) + 1));
+        }
+        this.players.push(newPlayer)
+        console.log(`${newPlayer.name} si è unito alla partita`);
+    },
+    'totalScore' : function () {
+        this.players.forEach(player=>{
+            let tot = player.scores.reduce((acc, num)=> acc + num)
+            player.totale=tot;
+        })
+        this.players.sort((a, b)=> b.totale - a.totale)
+    },
+    'winner' : function () {
+        let winner = this.players[0]
+        console.log(`VINCE ${winner.name} con ${winner.totale} punti`);        
+    },
+    'finalRanking' : function () {
+        console.log('CLASSIFICA FINALE:');
+        this.players.forEach(player => {
+            console.log(`${player.name} => ${player.totale} punti`);
+        })
     }
 }
 
 bowling.playersList();
 bowling.gameSimulator();
+bowling.addPlayer('Luigi');
+bowling.totalScore();
+bowling.finalRanking();
+bowling.winner();
 
